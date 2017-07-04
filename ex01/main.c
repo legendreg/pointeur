e5737b77b0662a6138df01c13562f847a16d15fa
glegendre <kubitusleroi@laposte.net>
2017-07-04 18:38:49 +0200

    lil

---
+++ ex01/main.c
@@ -0,0 +1,10 @@
+int main (void)
+{
+	int n1;
+	int n2;
+	n1 = 13;
+	n2 = 27;
+	myswapint(&n1,&n2);
+	printf("%d %d\n",n1,n2);
+	return(0);
+}
---
+++ ex01/myswap.c
@@ -0,0 +1,7 @@
+void myswapint (int *p1, int *p2)
+{
+	int r;
+	r = *p1;
+	*p1 = *p2;
+	*p2 = r;
+}
